ddb1b1648fd3dab32bff1bcc5e0357238ae90aff
Pasang Sherpa <pgsherpa12@gmail.com>
2015-05-24 14:26:44 -0700

    Docker image fixed to use entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,16 @@
 set -e
 
 # Add kibana as command if needed
-if [[ "$1" == -* ]]; then
+if [ "${1:0:1}" = '-' ]; then
     set -- kibana "$@"
 fi
 
-# Run as user "kibana" if the command is "kibana"
-if [ "$1" == kibana ]; then
-    set -- gosu kibana "$@"
+# Drop root privileges if we are running kibana
+if [ "$1" = 'kibana' ]; then
+    exec gosu $(whoami) "$@"
 fi
 
+# As argument is not related to kibana,
+# then assume that user wants to run his own process,
+# for example a `bash` shell to explore this image
 exec "$@"
